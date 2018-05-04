991fdc1d976a909abe2f549a109411456e03ba34
Elizabeth Rogers <elle.eels@gmail.com>
2018-05-04 01:57:53 -0700

    cleaning up

--- public/assets/js/friends.js
+++ public/assets/js/friends.js
@@ -142,15 +142,83 @@ princessArray: [{
     group: "princesses", 
     name: "Jasmine",
     scores: [
+        4,
         1,
         1,
+        4,
         1,
+        3,
+        5,
+        4,
+        2,
+        1
+    ]
+},
+{
+    photo: "./assets/images/belle.jpg", 
+    group: "princesses", 
+    name: "Belle",
+    scores: [
+        5,
         1,
+        4,
         1,
+        3,
         1,
+        3,
+        2,
+        2,
+        2
+    ]
+},
+{
+    photo: "./assets/images/snowwhite.jpg", 
+    group: "princesses", 
+    name: "Snow White",
+    scores: [
         1,
+        5,
+        5,
+        5,
+        5,
+        3,
         1,
+        3,
+        5,
+        5
+    ]
+},
+{
+    photo: "./assets/images/rapunzel.jpg", 
+    group: "princesses", 
+    name: "Rapunzel",
+    scores: [
         1,
+        3,
+        4,
+        5,
+        2,
+        5,
+        3,
+        3,
+        5,
+        3
+    ]
+},
+{
+    photo: "./assets/images/ariel.jpg", 
+    group: "princesses", 
+    name: "Ariel",
+    scores: [
+        4,
+        3,
+        3,
+        1,
+        4,
+        4,
+        1,
+        5,
+        4,
         1
     ]
 }
@@ -160,16 +228,84 @@ villainArray: [{
     group: "villains", 
     name: "Jafar",
     scores: [
+        5,
+        2,
+        1,
+        1,
+        4,
+        2,
+        1,
+        5,
+        1,
+        1
+    ]
+},
+{
+    photo: "./assets/images/ursula.jpg", 
+    group: "villains", 
+    name: "Ursula",
+    scores: [
+        3,
+        2,
+        3,
+        5,
+        1,
+        3,
+        4,
         3,
         3,
+        1
+    ]
+},
+{
+    photo: "./assets/images/evilqueen.jpg", 
+    group: "villains", 
+    name: "The Evil Queen",
+    scores: [
+        1,
+        5,
+        1,
+        1,
+        4,
+        4,
         3,
+        5,
+        1,
+        4
+    ]
+},
+{
+    photo: "./assets/images/gaston.jpg", 
+    group: "villains", 
+    name: "Gaston",
+    scores: [
+        1,
+        5,
+        2,
+        1,
+        1,
+        5,
+        2,
+        2,
+        4,
+        4
+    ]
+},
+{
+    photo: "./assets/images/mothergothel.jpg", 
+    group: "villains", 
+    name: "Mother Gothel",
+    scores: [
+        1,
+        4,
+        1,
+        4,
+        5,
         3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3
+        1,
+        5,
+        1,
+        1
     ]
 }
 ]
